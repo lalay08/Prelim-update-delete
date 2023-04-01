b4ba4e44c7ca6b11710027753d559fc4c8636049
lalay08 <115868735+lalay08@users.noreply.github.com>
2023-04-01 15:57:08 +0800

    Add files via upload

---
+++ Lalay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace malipang.Entities
+{
+    public partial class Lalay
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Age { get; set; } = null!;
+    }
+}
